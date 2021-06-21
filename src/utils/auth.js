ee4f4959465d6870f40545d14d62e847ca1b7ab0
风之忧伤 <575792372@qq.com>
2021-06-21 18:35:14 +0800

    add

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
+const TokenKey = 'VUE_ELEMENT_ADMIN_TOKEN'
 
 export function getToken() {
   return Cookies.get(TokenKey)
