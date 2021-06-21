c8553974b2580e001f7bde91835d0a0a0168165d
风之忧伤 <575792372@qq.com>
2021-06-21 10:19:23 +0800

    优化

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 
 // 防止点击同一个路由时候报错
-//获取原型对象上的push函数
 const originalPush = Router.prototype.push;
-//修改原型对象中的push方法
 Router.prototype.push = function push(location) {
   return originalPush.call(this, location).catch(err => err);
 };
